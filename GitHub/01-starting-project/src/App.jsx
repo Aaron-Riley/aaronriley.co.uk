4c1a9ace6a233a22164486964cfa5489d3612878
AaronG19 <72134243+Aaron-Riley@users.noreply.github.com>
2024-04-26 19:00:11 +0100

    First commit

---
+++ GitHub/01-starting-project/src/App.jsx
@@ -0,0 +1,12 @@
+import UserInput from './UserInput.jsx'
+import Result from './Result.jsx'
+function App() {
+  return (
+    <div>
+      <UserInput />
+      <Result />
+    </div>
+  )
+}
+
+export default App
---
+++ GitHub/01-starting-project/src/Result.jsx
@@ -0,0 +1,20 @@
+export default function Result() {
+    return <div id="result">
+        <table>
+            <thead id="thead">
+                <tbody id="tbody">
+                    <tr>
+                        <th>
+                            Test Header
+                        </th>
+                    </tr>
+                    <tr>
+                        <td>
+                            Test Description
+                        </td>
+                    </tr>   
+                </tbody>
+            </thead>
+        </table>
+    </div>
+}
