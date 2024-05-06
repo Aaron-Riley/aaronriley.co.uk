c04fd7356d78a870da5be3c5b7b12f57415ed376
AaronG19 <72134243+Aaron-Riley@users.noreply.github.com>
2024-05-06 15:58:14 +0100

    first commit/adding inline styles and CSS Modules for specific component styling

---
+++ GitHub/01-starting-project/src/InputGroup.jsx
@@ -0,0 +1,8 @@
+export default function InputGroup() {
+  return (
+    <div id="input-group">
+      <label>Initial Investment</label>
+      <input type="number"></input>
+    </div>
+  );
+}
---
+++ GitHub/01-starting-project/src/UserInput.jsx
@@ -0,0 +1,12 @@
+import InputGroup from "./InputGroup.jsx"
+
+export default function UserInput() {
+    return (
+        <div id="user-input">
+            <InputGroup />
+            <InputGroup />
+            <InputGroup />
+            <InputGroup />
+        </div>
+    )
+    }
